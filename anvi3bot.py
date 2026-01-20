990d27be25847725aa5de44f2b15d410b2ac5e3f
anviii85 <anvitaghorpade@gmail.com>
2026-01-20 15:30:28 +0530

    Update title from 'PragyanAI' to 'ANVIAI'

--- anvi3bot.py
+++ anvi3bot.py
@@ -2,7 +2,7 @@ import streamlit as st
 from groq import Groq
 
 st.set_page_config("PragyanAI Content Generator", layout="wide")
-st.title("📢 PragyanAI – Content Generator")
+st.title("📢 ANVIAI – Content Generator")
 
 client = Groq(api_key=st.secrets["GROQ_API_KEY"])
 
